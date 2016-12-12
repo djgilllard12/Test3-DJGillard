ae53abd8396b6340b77b5c463ceef00f8fca561e
djgilllard12 <djgillard12@live.ca>
2016-12-12 10:31:29 -0500

    Adding test

---
+++ Test3-DJGillard/Test3-DJGillard/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Test3_DJGillard
+{
+    partial class Test3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMyName = new System.Windows.Forms.Label();
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.lblOutputProduct = new System.Windows.Forms.Label();
+            this.lblErrorMessage1 = new System.Windows.Forms.Label();
+            this.lblErrorMessage2 = new System.Windows.Forms.Label();
+            this.btnProduct = new System.Windows.Forms.Button();
+            this.lblLoopChristmas = new System.Windows.Forms.Label();
+            this.btnLoopChristmas = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblLoginMessage = new System.Windows.Forms.Label();
+            this.lblLoopMovies = new System.Windows.Forms.Label();
+            this.btnLoopMovies = new System.Windows.Forms.Button();
+            this.cboSeason = new System.Windows.Forms.ComboBox();
+            this.cboMonths = new System.Windows.Forms.ComboBox();
+            this.btnShowMonths = new System.Windows.Forms.Button();
+            this.lblQ7 = new System.Windows.Forms.Label();
+            this.btnQ7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMyName
+            // 
+            this.lblMyName.AutoSize = true;
+            this.lblMyName.Location = new System.Drawing.Point(12, 21);
+            this.lblMyName.Name = "lblMyName";
+            this.lblMyName.Size = new System.Drawing.Size(502, 13);
+            this.lblMyName.TabIndex = 0;
+            this.lblMyName.Text = "My name is DJ(Derrick) Gillard, my student number is 200344037 and I am going to " +
+    "do great on this exam.\r\n";
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.Location = new System.Drawing.Point(26, 57);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.Size = new System.Drawing.Size(100, 20);
+            this.txtInput1.TabIndex = 1;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Location = new System.Drawing.Point(26, 83);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(100, 20);
+            this.txtInput2.TabIndex = 2;
+            // 
+            // lblOutputProduct
+            // 
+            this.lblOutputProduct.AutoSize = true;
+            this.lblOutputProduct.Location = new System.Drawing.Point(32, 143);
+            this.lblOutputProduct.Name = "lblOutputProduct";
+            this.lblOutputProduct.Size = new System.Drawing.Size(0, 13);
+            this.lblOutputProduct.TabIndex = 3;
+            // 
+            // lblErrorMessage1
+            // 
+            this.lblErrorMessage1.AutoSize = true;
+            this.lblErrorMessage1.ForeColor = System.Drawing.Color.Maroon;
+            this.lblErrorMessage1.Location = new System.Drawing.Point(142, 60);
+            this.lblErrorMessage1.Name = "lblErrorMessage1";
+            this.lblErrorMessage1.Size = new System.Drawing.Size(162, 13);
+            this.lblErrorMessage1.TabIndex = 4;
+            this.lblErrorMessage1.Text = "ERROR: Please Input an Integer";
+            this.lblErrorMessage1.Visible = false;
+            // 
+            // lblErrorMessage2
+            // 
+            this.lblErrorMessage2.AutoSize = true;
+            this.lblErrorMessage2.ForeColor = System.Drawing.Color.Maroon;
+            this.lblErrorMessage2.Location = new System.Drawing.Point(142, 86);
+            this.lblErrorMessage2.Name = "lblErrorMessage2";
+            this.lblErrorMessage2.Size = new System.Drawing.Size(162, 13);
+            this.lblErrorMessage2.TabIndex = 5;
+            this.lblErrorMessage2.Text = "ERROR: Please Input an Integer";
+            this.lblErrorMessage2.Visible = false;
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.Location = new System.Drawing.Point(35, 109);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Size = new System.Drawing.Size(75, 23);
+            this.btnProduct.TabIndex = 6;
+            this.btnProduct.Text = "Submit";
+            this.btnProduct.UseVisualStyleBackColor = true;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
+            // 
+            // lblLoopChristmas
+            // 
+            this.lblLoopChristmas.AutoSize = true;
+            this.lblLoopChristmas.Location = new System.Drawing.Point(26, 181);
+            this.lblLoopChristmas.Name = "lblLoopChristmas";
+            this.lblLoopChristmas.Size = new System.Drawing.Size(0, 13);
+            this.lblLoopChristmas.TabIndex = 7;
+            // 
+            // btnLoopChristmas
+            // 
+            this.btnLoopChristmas.Location = new System.Drawing.Point(29, 207);
+            this.btnLoopChristmas.Name = "btnLoopChristmas";
+            this.btnLoopChristmas.Size = new System.Drawing.Size(75, 23);
+            this.btnLoopChristmas.TabIndex = 8;
+            this.btnLoopChristmas.Text = "Begin Loop";
+            this.btnLoopChristmas.UseVisualStyleBackColor = true;
+            this.btnLoopChristmas.Click += new System.EventHandler(this.btnLoopChristmas_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(26, 256);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(35, 294);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 10;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblLoginMessage
+            // 
+            this.lblLoginMessage.AutoSize = true;
+            this.lblLoginMessage.ForeColor = System.Drawing.Color.Maroon;
+            this.lblLoginMessage.Location = new System.Drawing.Point(145, 262);
+            this.lblLoginMessage.Name = "lblLoginMessage";
+            this.lblLoginMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblLoginMessage.TabIndex = 11;
+            // 
+            // lblLoopMovies
+            // 
+            this.lblLoopMovies.AutoSize = true;
+            this.lblLoopMovies.Location = new System.Drawing.Point(366, 109);
+            this.lblLoopMovies.Name = "lblLoopMovies";
+            this.lblLoopMovies.Size = new System.Drawing.Size(0, 13);
+            this.lblLoopMovies.TabIndex = 12;
+            // 
+            // btnLoopMovies
+            // 
+            this.btnLoopMovies.Location = new System.Drawing.Point(369, 53);
+            this.btnLoopMovies.Name = "btnLoopMovies";
+            this.btnLoopMovies.Size = new System.Drawing.Size(75, 23);
+            this.btnLoopMovies.TabIndex = 13;
+            this.btnLoopMovies.Text = "Begin Loop Movies";
+            this.btnLoopMovies.UseVisualStyleBackColor = true;
+            this.btnLoopMovies.Click += new System.EventHandler(this.btnLoopMovies_Click);
+            // 
+            // cboSeason
+            // 
+            this.cboSeason.FormattingEnabled = true;
+            this.cboSeason.Items.AddRange(new object[] {
+            "Spring",
+            "Summer",
+            "Fall",
+            "Winter"});
+            this.cboSeason.Location = new System.Drawing.Point(369, 143);
+            this.cboSeason.Name = "cboSeason";
+            this.cboSeason.Size = new System.Drawing.Size(121, 21);
+            this.cboSeason.TabIndex = 14;
+            // 
+            // cboMonths
+            // 
+            this.cboMonths.FormattingEnabled = true;
+            this.cboMonths.Location = new System.Drawing.Point(369, 207);
+            this.cboMonths.Name = "cboMonths";
+            this.cboMonths.Size = new System.Drawing.Size(121, 21);
+            this.cboMonths.TabIndex = 15;
+            // 
+            // btnShowMonths
+            // 
+            this.btnShowMonths.Location = new System.Drawing.Point(384, 170);
+            this.btnShowMonths.Name = "btnShowMonths";
+            this.btnShowMonths.Size = new System.Drawing.Size(97, 23);
+            this.btnShowMonths.TabIndex = 16;
+            this.btnShowMonths.Text = "Show Months";
+            this.btnShowMonths.UseVisualStyleBackColor = true;
+            this.btnShowMonths.Click += new System.EventHandler(this.btnShowMonths_Click);
+            // 
+            // lblQ7
+            // 
+            this.lblQ7.AutoSize = true;
+            this.lblQ7.Location = new System.Drawing.Point(685, 53);
+            this.lblQ7.Name = "lblQ7";
+            this.lblQ7.Size = new System.Drawing.Size(0, 13);
+            this.lblQ7.TabIndex = 17;
+            // 
+            // btnQ7
+            // 
+            this.btnQ7.Location = new System.Drawing.Point(688, 75);
+            this.btnQ7.Name = "btnQ7";
+            this.btnQ7.Size = new System.Drawing.Size(75, 23);
+            this.btnQ7.TabIndex = 18;
+            this.btnQ7.Text = "Submit Q7";
+            this.btnQ7.UseVisualStyleBackColor = true;
+            this.btnQ7.Click += new System.EventHandler(this.btnQ7_Click);
+            // 
+            // Test3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 367);
+            this.Controls.Add(this.btnQ7);
+            this.Controls.Add(this.lblQ7);
+            this.Controls.Add(this.btnShowMonths);
+            this.Controls.Add(this.cboMonths);
+            this.Controls.Add(this.cboSeason);
+            this.Controls.Add(this.btnLoopMovies);
+            this.Controls.Add(this.lblLoopMovies);
+            this.Controls.Add(this.lblLoginMessage);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnLoopChristmas);
+            this.Controls.Add(this.lblLoopChristmas);
+            this.Controls.Add(this.btnProduct);
+            this.Controls.Add(this.lblErrorMessage2);
+            this.Controls.Add(this.lblErrorMessage1);
+            this.Controls.Add(this.lblOutputProduct);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtInput1);
+            this.Controls.Add(this.lblMyName);
+            this.Name = "Test3";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMyName;
+        private System.Windows.Forms.TextBox txtInput1;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.Label lblOutputProduct;
+        private System.Windows.Forms.Label lblErrorMessage1;
+        private System.Windows.Forms.Label lblErrorMessage2;
+        private System.Windows.Forms.Button btnProduct;
+        private System.Windows.Forms.Label lblLoopChristmas;
+        private System.Windows.Forms.Button btnLoopChristmas;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblLoginMessage;
+        private System.Windows.Forms.Label lblLoopMovies;
+        private System.Windows.Forms.Button btnLoopMovies;
+        private System.Windows.Forms.ComboBox cboSeason;
+        private System.Windows.Forms.ComboBox cboMonths;
+        private System.Windows.Forms.Button btnShowMonths;
+        private System.Windows.Forms.Label lblQ7;
+        private System.Windows.Forms.Button btnQ7;
+    }
+}
+
---
+++ Test3-DJGillard/Test3-DJGillard/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test3_DJGillard
+{
+    public partial class Test3 : Form
+    {
+        public Test3()
+        {
+            InitializeComponent();
+        }
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            // creating varaiables
+            Boolean formOk = true;
+            Int32 product=0;
+            Int32 num1 = Convert.ToInt32(txtInput1.Text);
+            Int32 num2 = Convert.ToInt32(txtInput2.Text);
+            // validating variables
+            if (Int32.TryParse(txtInput1.Text, out num1)== false)
+            {
+                lblErrorMessage1.Visible = true;
+                formOk = false;
+            }
+            if (Int32.TryParse(txtInput2.Text, out num2) == false)
+            {
+                lblErrorMessage2.Visible = true;
+                formOk = false;
+            }
+            else
+            {
+                // creating method
+                formOk = true;
+                lblOutputProduct.Text = multiplyNumbers(product);
+            }
+        }
+        private String multiplyNumbers(Int32 product)
+        {
+            // using method to calculate product and output it
+            String message;
+            Int32 num1 = Convert.ToInt32(txtInput1.Text);
+            Int32 num2 = Convert.ToInt32(txtInput2.Text);
+            product = num1 * num2;
+            message = Convert.ToString(product);
+            return message;
+        }
+
+
+        private void btnLoopChristmas_Click(object sender, EventArgs e)
+        {
+            // creating array
+            String[] christmas = new String[3];
+            christmas[0] = "Merry";
+            christmas[1] = "Christmas";
+            christmas[2] = "C# is great";
+            //looping array
+            for (var i = 0; i <= 2; i++)
+            {
+                lblLoopChristmas.Text += " " + christmas[i];
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String password = txtPassword.Text;
+            if (password == "frosty")
+            {
+                // outputing the correct colors and error message
+                lblLoginMessage.Text = "You entered the correct password";
+                lblLoginMessage.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblLoginMessage.Text = "You entered the incorrect password";
+                lblLoginMessage.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void btnLoopMovies_Click(object sender, EventArgs e)
+        {
+            String[,] movies = new String[3, 2];
+            // creating 2-D array
+            movies[0, 0] = "How the Grinch Stole Christmas";
+            movies[0, 1] = "1966";
+            movies[1, 0] = "Frosty the Snowman";
+            movies[1, 1] = "1969";
+            movies[2, 0] = "Arthur Christmas";
+            movies[2, 1] = "2011";
+            
+            // loop through the array
+            for (var i = 0; i<=2; i++)
+            {
+                lblLoopMovies.Text += Convert.ToString(movies[i, 0] + "-" + movies[i,1])+ " ";
+            }
+        }
+
+        private void btnShowMonths_Click(object sender, EventArgs e)
+        {
+            String season = Convert.ToString(cboSeason);
+            // deciding which months to fill second combobox with
+            if (season == ("Spring"))
+            {
+                cboMonths.Items.Add("March");
+                cboMonths.Items.Add("April");
+                cboMonths.Items.Add("May");
+            }
+            if (season == "Summer")
+            {
+                cboMonths.Items.Add("June");
+                cboMonths.Items.Add("July");
+                cboMonths.Items.Add("August");
+            }
+            if (season == "Fall")
+            {
+                cboMonths.Items.Add("September");
+                cboMonths.Items.Add("October");
+                cboMonths.Items.Add("November");
+            }
+            if (season == "Winter")
+            {
+                cboMonths.Items.Add("December");
+                cboMonths.Items.Add("January");
+                cboMonths.Items.Add("Febuary");
+            }
+
+        }
+
+        private void btnQ7_Click(object sender, EventArgs e)
+        {
+            // fixing broken code
+            Double x = 4;
+            Double y = 12;
+            Double result = x / y;
+            lblQ7.Text = Convert.ToString(result);
+        }
+    }
+} 
+
